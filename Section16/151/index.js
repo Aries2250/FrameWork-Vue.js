1d7216d4f0a23d4e874f173a9abf519860ed90fe
Artemiy <62555435+Aries2250@users.noreply.github.com>
2023-02-15 19:03:02 +0200

    Section 16 Full

---
+++ Section16/151/index.js
@@ -0,0 +1,4 @@
+// console.log("Hello World!");
+let text = 'Hello world';
+
+console.log(text);
---
+++ Section16/152/index.js
@@ -0,0 +1,6 @@
+// let Car = require('./car').Car/*Class*/;
+
+require('./car');
+
+let bmw = new /*global.*/Car('BMW');
+bmw.logName();
